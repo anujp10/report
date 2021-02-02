09d0bc9c383cd6479dcbae9fff1c29c43b3e909b
u58822 <u58822@login-2.aidevcloud>
2021-02-02 15:41:18 -0800

    fpga_reg

---
+++ fpga_reg/lib/product_data.js
@@ -0,0 +1,1 @@
+var infoJSON={"compileInfo":{"nodes":[{"family":"Arria 10, 10AX115S2F45I2SGES, intel_a10gx_pac:pac_a10","id":1,"name":"fpga_reg_registered_report","product":"SYCL","quartus":"","time":"Tue Feb  2 15:35:53 2021","version":"20.3.0 Build 168.5"}]}};
