ee48ed9747dae505ee4e34f11a9896efa0d2e1c3
u58822 <u58822@login-2.aidevcloud>
2021-02-02 17:58:49 -0800

    new

---
+++ max_concurrency/lib/product_data.js
@@ -0,0 +1,1 @@
+var infoJSON={"compileInfo":{"nodes":[{"family":"Arria 10, 10AX115S2F45I2SGES, intel_a10gx_pac:pac_a10","id":1,"name":"max_concurrency_report","product":"SYCL","quartus":"","time":"Tue Feb  2 17:56:15 2021","version":"20.3.0 Build 168.5"}]}};
