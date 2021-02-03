e64c5bd8ff0c3f5f5777a07061afc40a205fb7d5
u58822 <u58822@login-2.aidevcloud>
2021-02-02 16:16:21 -0800

    new_eg

---
+++ kernel_args_restrict/lib/product_data.js
@@ -0,0 +1,1 @@
+var infoJSON={"compileInfo":{"nodes":[{"family":"Arria 10, 10AX115S2F45I2SGES, intel_a10gx_pac:pac_a10","id":1,"name":"kernel_args_restrict_report","product":"SYCL","quartus":"","time":"Tue Feb  2 16:01:12 2021","version":"20.3.0 Build 168.5"}]}};
