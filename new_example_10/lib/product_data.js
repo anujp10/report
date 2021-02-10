9ab74869f1f52dc83d8ea4eb40da0cd8ef38161f
u58822 <u58822@login-2.aidevcloud>
2021-02-09 18:00:04 -0800

    1

---
+++ new_example_10/lib/product_data.js
@@ -0,0 +1,1 @@
+var infoJSON={"compileInfo":{"nodes":[{"family":"Arria 10, 10AX115S2F45I2SGES, intel_a10gx_pac:pac_a10","id":1,"name":"explicit_data_movement_report","product":"SYCL","quartus":"","time":"Tue Feb  9 17:57:54 2021","version":"20.3.0 Build 168.5"}]}};
